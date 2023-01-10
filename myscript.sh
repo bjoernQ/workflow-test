74b567832640d284f5441083f87c5f169cabcbe5
bjoernQ <bjoern.quentin@mobile-j.de>
2023-01-10 08:41:19 +0100

    Test

--- myscript.sh
+++ myscript.sh
@@ -1,2 +1,2 @@
-curl -sSv https://api.github.com/repos/esp-rs/rust-build/releases
-curl -sSv https://api.github.com/repos/esp-rs/rust-build/releases -H 'Authorization: $GITHUB_TOKEN'
+curl -sSv https://api.github.com/repos/esp-rs/rust-build/releases?per_page=1
+curl -sSv https://api.github.com/repos/esp-rs/rust-build/releases?per_page=1 -H 'Authorization: $GITHUB_TOKEN'
