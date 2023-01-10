e00e7620fc5b6878015399db1eb44d1f616206b2
bjoernQ <bjoern.quentin@mobile-j.de>
2023-01-10 09:03:14 +0100

    Test

--- myscript.sh
+++ myscript.sh
@@ -1,2 +1,9 @@
+echo "rust-build repo - no auth"
 curl -sSv https://api.github.com/repos/esp-rs/rust-build/releases?per_page=1
+echo "rust-build repo - auth"
 curl -sSv https://api.github.com/repos/esp-rs/rust-build/releases?per_page=1 -H 'Authorization: $GITHUB_TOKEN'
+
+echo "own repo - no auth"
+curl -sSv https://api.github.com/repos/bjoernQ/workflow-test/releases?per_page=1
+echo "own repo - auth"
+curl -sSv https://api.github.com/repos/bjoernQ/workflow-test/releases?per_page=1 -H 'Authorization: $GITHUB_TOKEN'
